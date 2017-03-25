50629c86b26e84730c73d978d4213b3fd27b5cb3
katthecat <kat.carsley@gmail.com>
2017-03-25 14:34:22 -0700

    Added project

---
+++ Unicorn/Unicorn/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Unicorn
+//
+//  Created by Kat Nasif on 03/25/2017.
+//  Copyright © 2017 Kat Nasif. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
